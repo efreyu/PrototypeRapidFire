f8102c59489c3472c5addbbdf6284c3dc578f552
Artur Barilo <efreyu@gmail.com>
2023-10-28 16:45:08 +0300

    master | player state machine /wip

--- src/gameplayModule/player/playerState.cpp
+++ src/gameplayModule/player/playerState.cpp
@@ -1,9 +1,41 @@
 #include "playerState.h"
 #include "godot_cpp/core/class_db.hpp"
+#include "godot_cpp/variant/utility_functions.hpp"
 
 using namespace rapidFire::gameplayModule;
 
 void playerState::_bind_methods() {
-    //    godot::ClassDB::bind_static_method( "GDExtensionTemplate", godot::D_METHOD( "version" ),
-    //                                       &GDExtensionTemplate::version );
+    // Methods
+    godot::ClassDB::bind_method(godot::D_METHOD("unhandled_input"), &playerState::unhandled_input);
+    godot::ClassDB::bind_method(godot::D_METHOD("process", "delta"), &playerState::process);
+    godot::ClassDB::bind_method(godot::D_METHOD("physics_process", "delta:float"), &playerState::physics_process);
+    godot::ClassDB::bind_method(godot::D_METHOD("enter", "msg:Dictionary"), &playerState::enter);
+    godot::ClassDB::bind_method(godot::D_METHOD("exit", "delta:float"), &playerState::exit);
+}
+
+void playerState::unhandled_input(const godot::Ref<godot::InputEvent>& event) {
+    //
+}
+
+void playerState::process(double delta) {
+    //
+}
+
+void playerState::physics_process(double delta) {
+    //
+}
+
+void playerState::enter(const godot::Dictionary& msg) {
+    //
+}
+
+void playerState::exit() {
+    //
+}
+
+void playerState::_ready() {
+    _parentState = cast_to<playerState>(get_parent());
+    if (_parentState) {
+        godot::UtilityFunctions::print("[node:", get_name(), "]playerState::_ready() - parent state: [node:", _parentState->get_name(), "]");
+    }
 }
--- src/gameplayModule/player/playerState.h
+++ src/gameplayModule/player/playerState.h
@@ -2,7 +2,9 @@
 #ifndef RAPIDFIRE_PLAYERSTATE_H
 #define RAPIDFIRE_PLAYERSTATE_H
 
+#include "godot_cpp/classes/input_event.hpp"
 #include "godot_cpp/classes/node.hpp"
+#include "godot_cpp/classes/ref.hpp"
 #include "godot_cpp/godot.hpp"
 
 namespace rapidFire::gameplayModule {
@@ -12,7 +14,16 @@ namespace rapidFire::gameplayModule {
         static void _bind_methods();
 
     public:
-        //
+        void unhandled_input(const godot::Ref<godot::InputEvent>& event);
+        void process(double delta);
+        void physics_process(double delta);
+        void enter(const godot::Dictionary& msg);
+        void exit();
+
+        // ovwerride section
+        void _ready() override;
+
+        playerState* _parentState = nullptr;
     };
 }// namespace rapidFire::gameplayModule
 
